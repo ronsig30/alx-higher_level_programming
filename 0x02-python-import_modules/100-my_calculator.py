1e9cd9982a0df04bdcd7471441a690da3c612f31
ronsig30 <ronaldrogony30@gmail.com>
2023-10-05 16:11:29 -0700

    calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    operators = {"+": add, "-": sub, "*": mul, "/": div}
+    operator = sys.argv[2]
+
+    if operator not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    if operator == "/":
+        if b == 0:
+            print("Error: Division by zero")
+            sys.exit(1)
+    result = operators[operator](a, b)
+    print("{} {} {} = {}".format(a, operator, b, result))
