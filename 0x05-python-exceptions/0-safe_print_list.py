ce0f17fa60b48d38d9de69f43e6de1528adf0f5d
ronsig30 <ronaldrogony30@gmail.com>
2023-10-23 15:26:52 +0300

    Elements of a given list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    number = 0
-    for i in range(x):
-        try:
+    i = 0
+    try:
+        while i is not x:
             print(my_list[i], end="")
-            number += 1
-        except IndexError:
-            break
-    print("")
-    Return
+            i += 1
+    except IndexError:
+        None
+    print()
+    return (i)
+
