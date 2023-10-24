0c37d73527a695148d0dae3ff8bc224ed4217727
ronsig30 <ronaldrogony30@gmail.com>
2023-10-25 02:47:48 +0300

    square instances

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -60,7 +60,7 @@ class Square:
     def __str__(self):
         """Print() is defined in the representation of a square."""
         if self.__size != 0:
-            [print("") for i on range(0, self.__position[1])]
+            [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
