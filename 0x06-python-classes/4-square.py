2a025d012013e25edab3bd0d828e3812b0a24b36
ronsig30 <ronaldrogony30@gmail.com>
2023-10-24 22:33:30 +0300

    Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Module of a square"""
+
+
+class Square:
+    """Defining a square"""
+
+    def __init__(self, size=0):
+        """Constructor.
+
+        Args:
+            size: length of a square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Property for the length of a square.
+
+        Raise:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
