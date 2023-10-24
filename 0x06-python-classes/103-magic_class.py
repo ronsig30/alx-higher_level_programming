c2c55c78ba32949d54ccb8277c0d47ac860b2669
ronsig30 <ronaldrogony30@gmail.com>
2023-10-25 02:11:54 +0300

    Reading from a bytecode

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""MagicClass matching exactly with bytecode provided"""
+
+import math
+
+
+class MagicClass:
+    """To represent a circle"""
+
+    def __init__(self, radius=0):
+        """MagicClass is initialized.
+        Arg:
+            radius (int or float): radius of new magicClass.
+        """
+        self.__radius = radius
+
+    def area(self):
+        """area of MagicClass is returned."""
+        return (self.__radius ** 2 * math.pi)
+
+    def circumference(self):
+        """circumference of Magicclass is returned."""
+        return (2 * math.pi * self.__radius)
