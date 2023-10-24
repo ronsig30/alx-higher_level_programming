9b0eea3d62d96c0913f8c90309a5293bf5b32e82
ronsig30 <ronaldrogony30@gmail.com>
2023-10-24 22:37:24 +0300

    Access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -30,3 +30,11 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    def area(self):
+        """Area of a square.
+
+        Return:
+            The size to be squared
+        """
+        return self.__size ** 2
